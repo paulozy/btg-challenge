1136eb0d36224a5efaaeabdcfe14e6496dd51f3c
Paulo Abreu <pa.aabreu18@gmail.com>
2024-07-01 19:22:31 -0300

    feat: create routes index file

--- order-ms/internal/infra/server/routes.go
+++ order-ms/internal/infra/server/routes.go
@@ -1,23 +1,36 @@
 package server
 
+import (
+	"github.com/paulozy/btg-challenge/order-ms/internal/infra/database/repositories"
+	"github.com/paulozy/btg-challenge/order-ms/internal/infra/server/controllers"
+	"github.com/paulozy/btg-challenge/order-ms/internal/usecases"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 var Routes = []Handler{}
 
-// func PopulateRoutes(mongoClient *mongo.Client) []Handler {
-// 	addOrderRoutes(mongoClient)
-// 	return Routes
-// }
+func PopulateRoutes(mongoClient *mongo.Client) []Handler {
+	addOrderRoutes(mongoClient)
+	return Routes
+}
 
-// func addOrderRoutes(mongoClient *mongo.Client) {
-// 	orderRepository := repositories.NewOrderRepository(mongoClient, "btg_challenge", "orders")
-// 	saveOrderUseCase := usecases.NewSaveOrderUseCase(orderRepository)
+func addOrderRoutes(mongoClient *mongo.Client) {
+	orderRepository := repositories.NewOrderRepository(mongoClient, "btg_challenges", "orders")
+	listOrdersByClientCodeUseCase := usecases.NewListOrdersByClientCodeUseCase(orderRepository)
 
-// 	orderUseCases := controllers.OrderUseCasesInput{
-// 		SaveOrderUseCase: saveOrderUseCase,
-// 	}
+	orderUseCases := controllers.OrderUseCasesInput{
+		ListOrdersByClientCodeUseCase: listOrdersByClientCodeUseCase,
+	}
 
-// 	orderController := controllers.NewOrderController(orderRepository, orderUseCases)
+	orderController := controllers.NewOrderController(orderRepository, orderUseCases)
 
-// 	orderControllerRoutes := []Handler{}
+	orderControllerRoutes := []Handler{
+		{
+			Path:   "/orders",
+			Method: "GET",
+			Func:   orderController.ListByClientCode,
+		},
+	}
 
-// 	Routes = append(Routes, orderControllerRoutes...)
-// }
+	Routes = append(Routes, orderControllerRoutes...)
+}
